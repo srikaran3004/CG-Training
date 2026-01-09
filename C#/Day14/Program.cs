19b27036fba73210312cdaefbe30c2d9c84cbd52
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-09 10:01:26 +0530

    Created today's folder

---
+++ C#/Day14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        FileInfo file = new FileInfo("sample.txt");
+        if (!file.Exists)
+        {
+            using (StreamWriter writer = file.CreateText())
+            {
+                writer.WriteLine("I'm Srikaran");
+            }
+        }
+        Console.WriteLine("File Name: " + file.Name);
+        Console.WriteLine("File Size: " + file.Length + " bytes");
+        Console.WriteLine("Created On: " + file.CreationTime);
+    }
+}
