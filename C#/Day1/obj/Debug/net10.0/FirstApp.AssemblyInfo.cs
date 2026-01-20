ed3a8d3ada5958df2f85835379d7a9f57d0db22f
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-20 06:59:23 +0530

    Updated Code

--- C#/Day1/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
+++ C#/Day1/obj/Debug/net10.0/FirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613acc809b4f6ed87c581594d1b730568077c63e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83747a6b723a8ccdebd3bd3609760542ab64212f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
