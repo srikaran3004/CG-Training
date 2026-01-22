429041012b1ef2afc97798a2fde21bf5fb5bbba1
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-22 14:38:17 +0530

    Added Saturday's Assignment

---
+++ C#/Saturday-Assessments/22nd_Jan/dllConsoleApp/dllConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+
+Assembly assembly = Assembly.Load("dllLibrary");
+Type[] types = assembly.GetTypes();
+
+foreach (var type in types)
+{
+    if (type.IsInterface)
+    {
+        Console.WriteLine($"\nInterface: {type.FullName}");
+    }
+    else if (type.IsClass)
+    {
+        Console.WriteLine($"\nClass: {type.FullName}");
+    }
+    else
+    {
+        Console.WriteLine($"\nType: {type.FullName}");
+    }
+
+    if (type.IsClass)
+    {
+        var interfaces = type.GetInterfaces();
+        foreach (var iface in interfaces)
+        {
+            Console.WriteLine($"  Implements: {iface.FullName}");
+        }
+    }
+
+    var methods = type.GetMethods(
+        BindingFlags.Public |
+        BindingFlags.NonPublic |
+        BindingFlags.Instance |
+        BindingFlags.Static 
+        //BindingFlags.DeclaredOnly
+    );
+
+    foreach (var method in methods)
+    {
+        Console.WriteLine($"  Method: {method.Name}");
+    }
+}
