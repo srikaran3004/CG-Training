a82e549e04e82b6f463e07bfa0068d43150da4db
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-30 12:29:44 +0530

    Added File_io

---
+++ C#/TopBrainsAssignments/File_io/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines("log.txt");
+        StreamWriter sw = new StreamWriter("error.txt");
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("ERROR"))
+                sw.WriteLine(line);
+        }
+        sw.Close();
+    }
+}
