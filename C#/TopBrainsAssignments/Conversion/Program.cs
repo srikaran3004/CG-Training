25f9bc71d8429c8f69441f6158e4cd8ff629a30d
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-30 12:32:59 +0530

    Adding Conversion

---
+++ C#/TopBrainsAssignments/Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static double FeetToCentimeters(int feet)
+    {
+        double cm = feet * 30.48;
+        return Math.Round(cm, 2, MidpointRounding.AwayFromZero);
+    }
+
+    public static void Main()
+    {
+        int ft = int.Parse(Console.ReadLine());
+        double res = FeetToCentimeters(ft);
+        Console.WriteLine(res);
+    }
+}
