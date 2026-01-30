2a32a42446f6b97c573b13b9fdc317adb16c3d2a
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-30 12:23:07 +0530

    MidPointRounding

---
+++ C#/TopBrainsAssignments/MidPointRounding/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        double r = Double.Parse(Console.ReadLine());
+        double area = Math.PI * r * r;
+        Console.WriteLine($"{area:F2}");
+    }
+}
