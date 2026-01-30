43b52cabd49f1d0a370b9e0764b13a3d6591deb3
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-01-30 12:18:33 +0530

    Added Swapping

---
+++ C#/TopBrainsAssignments/Swapping/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Swap(ref a, ref b);
+    }
+    static void Swap(ref int a, ref int b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+}
