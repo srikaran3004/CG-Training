a5d2d495fc41c2c1c33ec2bd985112ff49762288
Srikaran Devarakonda <srikaran2230@gmail.com>
2026-02-03 08:58:45 +0530

    Added CG_M1_Assessment

---
+++ C#/CG-M1_Assessment/FlipKey/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the word:");
+        string str = Console.ReadLine();
+
+        CleanseAndInverst ci = new CleanseAndInverst();
+        string result = ci.calculate(str);
+
+        Console.WriteLine(result);
+    }
+}
